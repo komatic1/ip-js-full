df5e38a05cc8d15f8b3a74bc63dfe8decbcf0e8f
Dima Dmitriev <komatic.dda@gmail.com>
2022-02-12 09:24:36 +0200

    4 First step

--- js/script.js
+++ js/script.js
@@ -1,7 +1,2 @@
-const spanTest = document.querySelector('#test'),
-    divP = document.querySelector('div:first-child');
 
-divP.addEventListener('click', () => {
-    spanTest.innerText = 'clicked';
-    spanTest.style.color = 'red';
-});
+console.log('1');

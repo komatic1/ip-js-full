872e66063aa1b7638bec630118ca31282a2a2df0
Dima Dmitriev <komatic.dda@gmail.com>
2022-02-16 18:38:51 +0200

    6 Data Types in JavaScript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 "use strict";
 
-console.log('1');
+// number
+let number = 4;
+let newNumber = 4.6;
+
+console.log('string' * 9);
+
+// string
+const person = 'Alex';
+
+// logic - boolean
+const bool = true;
+const nBool = false;
+
+// specific types
+console.log(something);
+
+// object
+const obj = {
+
+};
+
+const nobj = {
+    name: 'Andy',
+    age: 40
+};
+console.log(nobj);
+
+// array - it is also an object
+let arr = ['play', 'orange', 'dog', 'mother'];
+

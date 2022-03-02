21e57a2b830782855ea4d77b596f7a14cde4244a
Dima Dmitriev <komatic.dda@gmail.com>
2022-03-02 19:11:42 +0200

    20 Object descruction

--- t.js
+++ t.js
@@ -13,21 +13,40 @@
  *         programming: 'PHP, JavaSript, SQL, SIEMENS',
  *         sport: 'Billiard'
  *     },
- *     test: null
+ *     test: null,
+ *     makeTest: function() {
+ *         console.log('TEST');
+ *     }
  * };
+ * 
+ * obj2.makeTest();
  * 
  * delete obj2.test;
  * 
+ * let counter = 0;
  * for (let key in obj2) {
  *     if (typeof(obj2[key]) === 'object') {
  *         for (let i in obj[key]) {
  *             console.log(obj2[key][i]);
+ *             counter++;
  *         }
  *     } else {
  *         console.log(`Property ${key} is ${obj2[key]}`);
+ *         counter++;
  *     }
  * }
  * 
- * 13:05 in 20
+ * now counter - is our length
+ * object DOESN"T have length property
+ * 
+ * 
+ * 
+ * --------------------
+ * console.log(Object.keys(obj2).length);
+ * 
+ * 
+ * --------------------
+ * objects descruction
+ * const {programming, sport} = obj2.skills;
  * 
  */

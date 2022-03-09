b444672160e9598d67ad511551476b89353c7d33
Dima Dmitriev <komatic.dda@gmail.com>
2022-03-09 20:01:02 +0200

    21 Arrays and pseudoarrays, part 1

--- t.js
+++ t.js
@@ -1,5 +1,37 @@
 "use strict";
 
 /**
- * clear for 21 - arrays and pseudoarrays
+ * \
  */
+
+const arr = [1, 2, 3, 6, 8];
+/**
+arr.pop();
+
+console.log(arr);
+
+arr.push(13);
+console.log(arr);
+
+ * shif & unshift - not convenient because of array index
+ 
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}*/
+for (let value of arr) {
+    console.log(value);
+}
+// length = last index + 1
+const a = [2, 3, 4, 7];
+/*
+a[99] = 0;
+console.log(a.length);
+console.log(a);
+*/
+// result
+// 100
+// [ 2, 3, 4, 7, <95 empty items>, 0 ]
+//
+a.forEach(function (item, index, a) {
+    console.log(`${index}: ${item} inside ${a}`);
+});
